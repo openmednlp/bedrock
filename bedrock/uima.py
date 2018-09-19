64f63e6a6b6993787d91fbd98603fafd5e1c9b22
ritaac <rita.achermann@usb.ch>
2018-09-19 10:16:50 +0200

    modification imported reports
    modify imported report such that it will not be changed when UIMA xmi report is imported into Webanno

--- bedrock/uima.py
+++ bedrock/uima.py
@@ -8,47 +8,55 @@ class Ubertext():
         print(file_path)
 
         self.__set_text_from_report(file_path)
+        self.xmi_id = 0
+        self.sofa_id=12
 
-        ###if text contains " at befinning or end remove
-        #self.text.strip('"')
+        #preprocess such that webanno and spacy text the same
+        #side effect: loose structure of report (newline)
+        self.text_preproc = self.text_raw
+        self.text_preproc.strip('"')
+        self.text_preproc = self.text_preproc.replace("\n", " ")
+        self.text_preproc =' '.join( filter( len, self.text_preproc.split( ' ' ) ))
+
 
         nlp = spacy.load(lang)
-        self.spacy_doc = nlp(self.text)
+
+        self.spacy_doc = nlp(self.text_preproc)
 
 
         self._XML_HEADER_ = '<?xml version="1.0" encoding="UTF-8"?>'
 
         ###RA add empty space separator
         self._UIMA_XMI_BEGIN_ = ' '.join(['<xmi:XMI',
-                                         'xmlns:pos="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos.ecore"',
-                                         'xmlns:tcas="http:///uima/tcas.ecore"',
-                                         'xmlns:xmi="http://www.omg.org/XMI"',
-                                         'xmlns:cas="http:///uima/cas.ecore"',
-                                         'xmlns:tweet="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos/tweet.ecore"',
-                                         'xmlns:morph="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/morph.ecore"',
-                                         'xmlns:dependency="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/dependency.ecore"',
-                                         'xmlns:type5="http:///de/tudarmstadt/ukp/dkpro/core/api/semantics/type.ecore"',
-                                         'xmlns:type7="http:///de/tudarmstadt/ukp/dkpro/core/api/transform/type.ecore"',
-                                         'xmlns:type6="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type.ecore"',
-                                         'xmlns:type2="http:///de/tudarmstadt/ukp/dkpro/core/api/metadata/type.ecore"',
-                                         'xmlns:type3="http:///de/tudarmstadt/ukp/dkpro/core/api/ner/type.ecore"',
-                                         'xmlns:type4="http:///de/tudarmstadt/ukp/dkpro/core/api/segmentation/type.ecore"',
-                                         'xmlns:type="http:///de/tudarmstadt/ukp/dkpro/core/api/coref/type.ecore"',
-                                         'xmlns:constituent="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/constituent.ecore"',
-                                         'xmlns:chunk="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/chunk.ecore"',
-                                         'xmlns:custom="http:///webanno/custom.ecore"',
-                                         'xmi:version="2.0">'])
+                                          'xmlns:pos="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos.ecore"',
+                                          'xmlns:tcas="http:///uima/tcas.ecore"',
+                                          'xmlns:xmi="http://www.omg.org/XMI"',
+                                          'xmlns:cas="http:///uima/cas.ecore"',
+                                          'xmlns:tweet="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos/tweet.ecore"',
+                                          'xmlns:morph="http:///de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/morph.ecore"',
+                                          'xmlns:dependency="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/dependency.ecore"',
+                                          'xmlns:type5="http:///de/tudarmstadt/ukp/dkpro/core/api/semantics/type.ecore"',
+                                          'xmlns:type7="http:///de/tudarmstadt/ukp/dkpro/core/api/transform/type.ecore"',
+                                          'xmlns:type6="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type.ecore"',
+                                          'xmlns:type2="http:///de/tudarmstadt/ukp/dkpro/core/api/metadata/type.ecore"',
+                                          'xmlns:type3="http:///de/tudarmstadt/ukp/dkpro/core/api/ner/type.ecore"',
+                                          'xmlns:type4="http:///de/tudarmstadt/ukp/dkpro/core/api/segmentation/type.ecore"',
+                                          'xmlns:type="http:///de/tudarmstadt/ukp/dkpro/core/api/coref/type.ecore"',
+                                          'xmlns:constituent="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/constituent.ecore"',
+                                          'xmlns:chunk="http:///de/tudarmstadt/ukp/dkpro/core/api/syntax/type/chunk.ecore"',
+                                          'xmlns:custom="http:///webanno/custom.ecore"',
+                                          'xmi:version="2.0">'])
 
-        self.cas = '<cas:NULL xmi:id="0"/>'
+        self.cas = '<cas:NULL xmi:id="' + str(self.xmi_id) + '"/>'
+        self.xmi_id += 1
 
-        ### To do replace with correct items
         self.doc_meta = ' '.join([
-                               '<type2:DocumentMetaData xmi:id="1" sofa="12" begin="0" end="' + str(len(self.spacy_doc.text)) + '" '
-                               'language="x-unspecified" documentTitle="None" documentId="spacy"',
-                               'documentUri ="' + file_path + '"',
-                               'collectionId = "file:/srv/webanno/repository/project/1/document/31/source/"',
-                               'documentBaseUri = "file:/srv/webanno/repository/project/1/document/31/source/"',
-                               'isLastSegment = "false"/>'])
+            '<type2:DocumentMetaData xmi:id="1" sofa="' + str(self.sofa_id) + '" begin="0" end="' + str(len(self.spacy_doc.text)) + '" ',
+            'language="x-unspecified" documentTitle="None" documentId="spacy nlp"',
+            'documentUri ="' + file_path + '"',
+            'collectionId = "' + file_path + '"',
+            'documentBaseUri = "' + file_path + '"',
+            'isLastSegment = "false"/>'])
 
         self.sofa = '<cas:Sofa xmi:id="{sofa}" sofaNum="1" sofaID="_InitialView" mimeType="text" sofaString="{sofa_string}"/>'
 
@@ -74,25 +82,37 @@ class Ubertext():
         rows.append(self.doc_meta)
         rows.append('\n')
 
+        token_list = [(token.text, token.idx, len(token.text), token.is_sent_start, token_id)
+                  for token_id, token in enumerate(self.spacy_doc)]
 
+        sentence_list=[]
 
-        sofa = 12 # TODO 'sofa': ???
-        tmp = {'sofa': 12, 'sofa_string': self.spacy_doc.text}
+        token_nr =len(token_list)
+        for t in range(0, token_nr):
+            tmp = {'id': token_list[t][4]+2, 'sofa': self.sofa_id, 'begin': token_list[t][1],
+                   'end': token_list[t][1] + token_list[t][2] }
+            rows.append(self.token.format(**tmp))
+            rows.append('\n')
+            self.xmi_id +=1
+            if token_list[t][3]==True or t==0:
+                sentence_list.append(token_list[t])
 
-
-        xmi_id  = 2
-        for j, sent in enumerate(self.spacy_doc.sents):
-            tmp = {'id': xmi_id, 'sofa': sofa, 'begin': sent.start_char, 'end': sent.start_char + len(sent.text)}
+        ###To do: replace with name (tuple) instead index, readability
+        for s in range(0, len(sentence_list)):
+            if s < len(sentence_list)-1:
+                tmp = {'id': self.xmi_id, 'sofa': self.sofa_id, 'begin': sentence_list[s][1],
+                       'end': sentence_list[s+1][1]-1 }
+            else:
+                tmp = {'id': self.xmi_id, 'sofa': self.sofa_id, 'begin': sentence_list[s][1],
+                       'end': len(self.spacy_doc.text) }
+            self.xmi_id += 1
             rows.append(self.sentence.format(**tmp))
             rows.append('\n')
-            xmi_id+=1
-            for i, tok in enumerate(sent):
-                tmp = {'id': xmi_id, 'sofa': sofa, 'begin': tok.idx, 'end': tok.idx + tok.__len__()}
-                rows.append(self.token.format(**tmp))
-                rows.append('\n')
-                xmi_id += 1
-        tmp = {'sofa': sofa, 'member_list': ' '.join([str(xmi_id) for xmi_id in range(1, xmi_id)])}
-        rows.append(self.sofa.format(sofa=sofa, sofa_string=self.spacy_doc.text))
+
+        tmp = {'sofa': self.sofa_id, 'member_list': ' '.join([str(self.xmi_id)
+                for self.xmi_id in range(1, self.xmi_id)])}
+
+        rows.append(self.sofa.format(sofa=self.sofa_id, sofa_string= self.spacy_doc.text))
         rows.append('\n')
         rows.append(self.view.format(**tmp))
         rows.append('\n')
@@ -100,9 +120,10 @@ class Ubertext():
         rows.append('\n')
         return ''.join(rows)
 
+
     def __set_text_from_report(self, file_path):
         with open(file_path, 'r') as f:
-            self.text = f.read()
+            self.text_raw = f.read()
 
     def __set_json(self, file_path):
         with open(file_path, 'r') as f:
@@ -117,10 +138,10 @@ class Ubertext():
         full_text = self.__get_text_from_json()
         doc_metadata = self.json['_views']['_InitialView']['DocumentMetaData'][0]
         return (
-            ' - Short info - \n' +
-            '\nTitle: ' + doc_metadata['documentTitle'] +
-            '\nId: ' + doc_metadata['documentId'] +
-            '\n\nFull string:\n' + full_text
+                ' - Short info - \n' +
+                '\nTitle: ' + doc_metadata['documentTitle'] +
+                '\nId: ' + doc_metadata['documentId'] +
+                '\n\nFull string:\n' + full_text
         )
 
     def persist_xmi(self, file_path):
@@ -145,21 +166,19 @@ def load_ubertext(file_path):
     import bedrock.common
     return bedrock.common.load_pickle(file_path)
 
+
 if __name__ == '__main__':
 
     import os as os
 
-    ###to be modified
     file_dir = '/home/achermannr/drivei_nlp_syn/Pathology/TestLabeling/Test/'
+    spacy_model = '/home/achermannr/nlp_local/library/de_core_news_sm-2.0.0/de_core_news_sm/de_core_news_sm-2.0.0'
+
     file_names =  [f for f in os.listdir(file_dir) if f.endswith('.txt')]
 
-    for i in range(0,file_names.__len__()):
-        utx = Ubertext(file_dir + file_names[i],
-                lang='/home/achermannr/nlp_local/library/de_core_news_sm-2.0.0/de_core_news_sm/de_core_news_sm-2.0.0')
+    for i in range(0, len(file_names)):
+        utx = Ubertext(file_dir + file_names[i], lang=spacy_model)
         utx.create_xmi_string_from_spacy()
-        #utx.persist_xmi('/home/marko/test_xmi.xmi')
         utx.persist_xmi( file_dir + file_names[i].replace('.txt', '.xmi'))
 
-    # TO do Text surrounded with " will not work need to remove beginning and ending "
-    # Sentence splitting not good enough?     
 
