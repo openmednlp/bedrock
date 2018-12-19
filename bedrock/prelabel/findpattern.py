9e24ebd9046be0db68e991b0552e9c097f94af98
marko <marko.obradovic@usb.ch>
2018-12-19 11:27:30 +0100

    added prelabeling

---
+++ bedrock/prelabel/findpattern.py
@@ -0,0 +1,18 @@
+import re
+
+
+def findpattern(regex, text):
+    
+    reg = re.compile(regex)
+
+    vec = []
+    match = []
+    string = ''
+
+    for el in re.finditer(reg, text):
+        if el:
+            indices = el.span()
+            vec.append(indices)
+            match.append(el.group())
+            string = string + text[indices[0]:indices[1]] + ', '
+    return vec, match
