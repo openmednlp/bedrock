fb18fa465d28005d35cff0f3c9cd61dc2391d504
obradovicma <37619856+obradovicma@users.noreply.github.com>
2019-03-13 15:56:56 +0100

    add alternative get_wideformat() method

--- bedrock/doc/doc.py
+++ bedrock/doc/doc.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandasql as pdsql
 
 from doc.annotation import Annotation
 from doc.layer import Layer
@@ -131,6 +132,7 @@ class Doc:
         return cas
 
     def get_wideformat(self, layers:List[str]) -> pd.DataFrame:
+        #ToDo file name, sentence number
         wideformat = self.__tokens.copy(deep=True)
         for layer in layers:
             feature_names = self.__annotations[
@@ -142,8 +144,48 @@ class Doc:
                     [Annotation.BEGIN, Annotation.END, Annotation.FEATURE_VAL]
                 ]
                 tmp.rename(columns={Annotation.FEATURE_VAL: Layer.TUMOR + '.' + feature_name}, inplace=True)
+
                 wideformat = pd.merge(wideformat, tmp, on=[Relation.BEGIN, Relation.END], how='left')
                 wideformat = wideformat.replace({pd.np.nan: None})
+        return wideformat
+
+    def get_wideformat_all(self):
+        '''
+
+              :ivar: tokens and annotations class members
+
+              :Restrictions:
+              Assumes that a token can have only one feature.value assigned from the same feature. In case of multiple
+              annotations will be concatinated.
+              An annotation is assigned to a token even it does not span the whole token, the wide format does not reflect
+              partial annotations of a token.
+
+              :return: table of tokens and their annotations in wide format
+              '''
+
+        if self.__annotations.empty == False:
+
+            anno = self.__annotations
+            token = self.__tokens
+            tmp_col_name = 'feature_value_con'
+
+            sqlanno =''.join(['select anno.layer, anno.' , Annotation.FEATURE , ', token.' , \
+                         Token.ID , ',group_concat(' , Annotation.FEATURE_VAL, ') ' , tmp_col_name , ' ', \
+                        'from anno anno inner join token token on anno.' , Annotation.BEGIN , \
+                        ' < token.' , Token.END , ' and anno.' , Annotation.END , ' > token.' , Token.BEGIN , \
+                        ' where anno.', Annotation.LAYER ,' NOT IN ("POS","Token", "Sentence") ' ,
+                        ' group by token.' , Token.ID , ', ' , Annotation.LAYER , ', ' , Annotation.FEATURE])
+
+            tmp_anno_token_df = pdsql.sqldf(sqlanno, locals())
+            tmp_anno_token_df.loc[:, 'col_pivot'] = tmp_anno_token_df[Annotation.LAYER] + "." + \
+                                                    tmp_anno_token_df[Annotation.FEATURE].fillna('')
+
+            tmp_anno_token_piv_df = tmp_anno_token_df.pivot(index='id', values=tmp_col_name, columns='col_pivot')
+
+            wideformat = token.merge(tmp_anno_token_piv_df, left_on='id', right_index=True, how='left')
+            wideformat.insert(0, 'doc_id', self.get_filename())
+
+
         return wideformat
 
     def write_xmi(self, file_name, typesystem_filepath):
